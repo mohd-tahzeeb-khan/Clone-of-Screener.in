1bcb3fb624ab949d003ecfe0af6ca6430658a0fa
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2024-04-30 02:17:08 +0530

    working on navbar

--- components/navbar.py
+++ components/navbar.py
@@ -8,12 +8,12 @@ navbar = html.Nav(id='navbar', className='navbar', children=[
         html.Li(id='title_li', className='nav-item', children=[
             html.A(id='title_link', className='nav-link', children='SCREENS', href='www.kkp.com'),
         ]),
-        dcc.Dropdown(['tahzeeb', 'saniya', 'dudh wali'], placeholder='TOOLS', className='dropdownmenu', optionHeight=30, maxHeight=250),
+        dcc.Dropdown(['tahzeeb', 'saniya', 'dudh wali'], placeholder='TOOLS', className='dropdownmenu', optionHeight=30, maxHeight=250, searchable=False,clearable=False,style={'backgroundColor': 'transparent', 'border':'none', 'color':'#000'}),
             
         ]),
         html.Div(id='rightside', className='rightside', children=[
-            html.Div(id='divlogin', className='divlogin', children=[html.A(id='btnlogin', className='btnlogin', children=['Login'],
-            href='www.tahzeeb.k'),
+            
+            html.Div(id='divlogin', className='divlogin', children=[html.Img(id='userimg', className='userimg', children=[], src='assets/user.png', alt='User'),html.A(id='btnlogin', className='btnlogin', children=['Login'], href='www.tahzeeb.k'),
         ]),
         html.Div(id='divgetfreeaccount', className='divgetfreeaccount', children=[html.A(id='btngetfreeaccount', className='btngetfreeaccount', children=['Get Free Account'],
             href='www.tahzeeb.k')
