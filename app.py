eb8d83b3a980338515058f7f1a6c45eab69d52f3
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2024-04-30 03:58:11 +0530

    footer component is integrated with main app callled app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from dash import Dash, html
 from components.navbar import navbar
+from components.footer import footer
 app=Dash(__name__)
 app.layout=html.Div([
-    navbar
+    navbar,
+    footer
 ])
 if __name__=='__main__':
     app.run(debug=True)
