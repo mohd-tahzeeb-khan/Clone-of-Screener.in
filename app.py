ffe74a62682de1bf26a5de99a751950a3f23101d
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2024-05-01 02:37:37 +0530

    added some meta data, and head tag to the app.py file

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from dash import Dash, html
 from components.navbar import navbar
 from components.footer import footer
 from components.landingpage import landing
-app=Dash(__name__)
+app=Dash(__name__, title='Stock Screener and fundamental analysis tool for Indian Stock')
 
 app.layout=html.Div([
     navbar,
