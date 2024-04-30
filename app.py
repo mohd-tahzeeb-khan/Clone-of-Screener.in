b5d09ffc6aca63ca0cf6ec1bcb11179ed2d4fa63
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2024-04-30 17:49:26 +0530

    landing-page is integrated in app.py

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from dash import Dash, html
 from components.navbar import navbar
 from components.footer import footer
+from components.landingpage import landing
 app=Dash(__name__)
+
 app.layout=html.Div([
     navbar,
+    landing,
     footer
 ])
 if __name__=='__main__':
