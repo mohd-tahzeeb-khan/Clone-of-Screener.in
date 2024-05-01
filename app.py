d1327f269d12e272ee0c07ff185de061f5450daf
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2024-05-02 02:57:53 +0530

    developing integrating design

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from dash import Dash, html
 from landingpage import landingpage
-app=Dash(__name__, title='Stock Screener and fundamental analysis tool for Indian Stock')
+from components.industry_info import section_1
+external_stylesheets = ['assets/info.css']
 
-app.layout=html.Div([])
+app=Dash(__name__, external_stylesheets=external_stylesheets,title='Stock Screener and fundamental analysis tool for Indian Stock')
+
+app.layout=html.Div([section_1])
 if __name__=='__main__':
     app.run(debug=True)
