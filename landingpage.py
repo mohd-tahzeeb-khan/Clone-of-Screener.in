17ad9ab1e4f0f481e531d95c3e050247539bbb17
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2024-05-01 17:36:47 +0530

    another file for landing page with integrated all the pages to it

---
+++ landingpage.py
@@ -0,0 +1,5 @@
+from dash import Dash, html
+from components.navbar import navbar
+from components.footer import footer
+from components.landingpage import landing
+landingpage=html.Div(id='', className='', children=[navbar,landing,footer])
