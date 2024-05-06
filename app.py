ea96bf16790193e341debc8b05294c52dccbaa34
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2024-05-06 23:42:50 +0530

    working on tables

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from dash import Dash, html
 from landingpage import landingpage
-from components.industry_info import section_1, Pros_Cons
+from components.industry_info import section_1, Pros_Cons, peer_table
 external_stylesheets = ['assets/info.css']
 
 app=Dash(__name__, external_stylesheets=external_stylesheets,title='Stock Screener and fundamental analysis tool for Indian Stock')
 
-app.layout=html.Div([section_1, Pros_Cons ])
+app.layout=html.Div([section_1, Pros_Cons, peer_table])
 if __name__=='__main__':
     app.run(debug=True)
