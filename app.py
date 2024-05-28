68feec765b822c8f046f5836e9c4fcacb8865546
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2024-05-28 17:49:28 +0530

    app.py file edited

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from dash import Dash, html
 from landingpage import landingpage
 from components.industry_info import section_1, Pros_Cons, peer_table, Profit_Loss, Quarterly_Results
+from components.footer import footer
 external_stylesheets = ['assets/info.css']
 
 app=Dash(__name__, external_stylesheets=external_stylesheets,title='Stock Screener and fundamental analysis tool for Indian Stock')
 
-app.layout=html.Div([section_1, Pros_Cons, peer_table, Profit_Loss, Quarterly_Results])
+app.layout=html.Div([section_1, Pros_Cons, peer_table, Quarterly_Results, Profit_Loss, footer])
 if __name__=='__main__':
     app.run(debug=True)
